aa46fdbeefad55bc40313399e82599dbcd4f06bb
Angitha <apurushothaman@premiergp.com>
2018-07-25 12:40:39 +0530

    final project Signed-off-by: Angitha <apurushothaman@premiergp.com>

--- insert.php
+++ insert.php
@@ -1,32 +1,27 @@
 
 <?php
    include('newentry.php');
-   
-
- $regemail=$_POST['newregemail'];
-  
-      $view="Select * from emp_details where email='$regemail'";
-      $result=mysqli_query($con,$view);
-      if(mysqli_num_rows($result)>0)
-        {
+   $regemail=$_POST['newregemail'];
+   $view="Select * from emp_details where email='$regemail'";
+   $result=mysqli_query($con,$view);
+     if(mysqli_num_rows($result)>0)
+       {
          echo "<script>dbfail();</script>";
+       }
+     else
+       {
+         $regname=$_POST['newregname'];
+   		 $regtitle=$_POST['newregtitle'];
+   		 $regemail=$_POST['newregemail'];
+   		 $view="INSERT INTO emp_details (name, jobtitle, email) VALUES ('$regname', '$regtitle', '$regemail')";
+   		 if(mysqli_query($con,$view))
+           {
+       	     echo "<script>dbsuccess();</script>";
+	 	   } 
+   		 else
+     	   {
+       	     echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+     	   }
         }
-      else
-        {
-          $regname=$_POST['newregname'];
-   		  $regtitle=$_POST['newregtitle'];
-   		  $regemail=$_POST['newregemail'];
-   		  $view="INSERT INTO emp_details (name, jobtitle, email) VALUES ('$regname', '$regtitle', '$regemail')";
-   		  if(mysqli_query($con,$view))
-     		{
-       			echo "<script>dbsuccess();</script>";
-	 		} 
-   		  else
-     		{
-       			echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-     		}
-        }
-  
-
 ?>
 
